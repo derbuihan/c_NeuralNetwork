dac52bd32e0bd8941ac9e9529bc7449289cf4e54
derbuihan <derbuihan@gmail.com>
2025-01-05 06:34:59 +0900

    Remove commented-out cross-entropy loss implementation for clarity and maintainability

--- src/loss.c
+++ src/loss.c
@@ -61,28 +61,3 @@ Loss *new_cross_entropy_loss(int batch_size, int class_num) {
   loss->backward = backward_cross_entropy_loss;
   return loss;
 }
-
-// Loss *loss_fn = new_cross_entropy_loss();
-// double loss = loss_fn->forward(loss_fn, y_true_batch, y_pred);
-// loss_fn->backward(loss_fn);
-//
-// double cross_entropy_loss(const Matrix *y_true, const Matrix *y_pred) {
-//
-//   Matrix *y_pred_softmax = new_matrix(y_pred->rows, y_pred->cols);
-//   softmax_matrix(y_pred_softmax, y_pred);
-//
-//   double loss = 0;
-//   double epsilon = 1e-15;
-//   for (int i = 0; i < y_true->rows; i++) {
-//     for (int j = 0; j < y_true->cols; j++) {
-//       if (y_true->elements[i * y_true->cols + j] > 0) {
-//         double prob =
-//             y_pred_softmax->elements[i * y_pred_softmax->cols + j] + epsilon;
-//         loss += y_true->elements[i * y_true->cols + j] * log(prob);
-//       }
-//     }
-//   }
-//
-//   free_matrix(y_pred_softmax);
-//   return -loss / y_true->rows;
-// }
